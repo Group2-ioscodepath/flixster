79fff1ab903d0d7b3f9d2c9ba703a49fc35f6451
Aman Dhruva Thamminana <dhruva.aman@gmail.com>
2020-09-26 13:52:06 -0400

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Aman Dhruva Thamminana on 9/26/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
